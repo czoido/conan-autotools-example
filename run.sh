ca02733a66a863e5be1d9800725fc7f3826208b3
czoido <mrgalleta@gmail.com>
2022-02-28 09:26:58 +0100

    wip

---
+++ run.sh
@@ -0,0 +1,9 @@
+autoreconf --install
+conan install . --install-folder=conan-env
+source conan-env/conanautotoolstoolchain.sh
+source conan-env/conanautotoolsdeps.sh
+./configure
+make
+src/helloworld 
+source deactivate_conanautotoolstoolchain.sh
+source deactivate_conanautotoolsdeps.sh
