e6a57362038cf0023e897a965e1aa6fe0da7e30d
czoido <mrgalleta@gmail.com>
2022-02-28 10:04:54 +0100

    wip

--- configure-flags-pkg-config.sh
+++ configure-flags-pkg-config.sh
@@ -1,3 +1,3 @@
 export CPPFLAGS="$CPPFLAGS $(pkg-config --cflags zlib)"
-export LIBS="$LIBS pkg-config --libs-only-l zlib"
+export LIBS="$LIBS $(pkg-config --libs-only-l zlib)"
 export LDFLAGS="$LDFLAGS $(pkg-config --libs-only-L --libs-only-other zlib)"
